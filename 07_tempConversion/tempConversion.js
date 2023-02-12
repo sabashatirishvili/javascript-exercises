850a06c831804e63630894e9dbbe00d47d67f1b8
Saba Shatirishvili <shatirishvilis1@gmail.com>
2023-02-12 21:11:56 +0400

    1-7 Done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+	let celsius = (temp - 32) * (5/9);
+	let rounded = Math.round(celsius * 10) / 10;
+	return rounded;
 };
 
 const convertToFahrenheit = function() {
+	let fahrenheit = temp * (5/9) + 32;
+	let rounded = Math.round(fahrenheit * 10) / 10;
+	return rounded;
 };
-
+console.log(convertToCelsius(211));
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
